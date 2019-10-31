d99f04df135a228e22da7a2764f91d331cd55ff8
novium <mail@novium.pw>
2019-10-31 18:31:26 +0100

    Integrate API and Web interface

--- web_ui/html/app.js
+++ web_ui/html/app.js
@@ -4,23 +4,55 @@ function sleep(ms) {
 
 async function createNewJob() {
     const fadeTime = 400;
+    let result;
 
     $('#create-job').fadeOut(fadeTime);
-    await sleep(fadeTime);
-    $('#wait-job').fadeIn(fadeTime)
-    await sleep(fadeTime);
-
-    for(let i = 0; i <= 100; i += 10) {
-        $('#progress-bar').width(i + '%');
-        await sleep(350);
-    }
-    $('#progress-bar').addClass('bg-success');
+    let create_job = await $.get('http://api.dev.cloud.novium.pw/create_job?angle=' + $('angle-input').val());
+    create_job = JSON.parse(create_job);
     
-    await sleep(400);
+    if(create_job.status != 'done') {
+        await sleep(fadeTime);
+        $('#wait-job').fadeIn(fadeTime)
+        await sleep(fadeTime);
 
-    $('#wait-job').fadeOut(fadeTime);
-    await sleep(fadeTime);
+        /* for(let i = 0; i <= 100; i += 10) {
+            $('#progress-bar').width(i + '%');
+            await sleep(1000);
+        } */
+        let result;
+        let w = 20;
+        while(true) {
+            result = $.get('http://api.dev.cloud.novium.pw/get?id=' + current_job.id);
+            result = JSON.parse(result);
+
+            if(result.status == 'created') {
+                $('#progress-bar').addClass('bg-secondary');
+                $('#progress-bar').width('20%');
+            }
+            if(result.status == 'computing') {
+                if(w < 90) {
+                    w += 10;
+                }
+                
+                $('#progress-bar').addClass('bg-warning');
+                $('#progress-bar').width(w + '%');
+            }
+            if(result.status == 'done') {
+                break;
+            }
+        }
+        $('#progress-bar').addClass('bg-success');
+
+        await sleep(400);
+
+        $('#wait-job').fadeOut(fadeTime);
+        await sleep(fadeTime);
+    } else {
+        result = create_job;
+    }
+
     $('#results').fadeIn(fadeTime);
+    $('#url').text(result.url);
 
     return false;
 }
